6d60168521e091652e96b1f2f9b27164475cfee0
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-03-20 15:30:58 +0530

    Change navbar background to dark and setup word count, char count and text preview

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,19 +7,36 @@ export default function TextForm(props) {
         let newText=text.toUpperCase()
         setText(newText)
     }
+
+    const handleLoClick = ()=>{
+        let newText=text.toLowerCase()
+        setText(newText)
+    }
+
     const handleOnChange = (event)=>{
         console.log("On Change")
         setText(event.target.value)        
     }
-  const [text,setText]=useState("Enter text here2")
+
+  const [text,setText]=useState("")
   return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">                
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+      <>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">                
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-        </div>
+            <div className="conatiner my-3">
+               <h2>Your Text Summary</h2> 
+               <p> {text.split(" ").length-1} words and {text.length} characters</p>
+               <p>{0.008*text.split(" ").length} Minutes read</p>
+               <h2>Preview</h2>
+               <p>{text}</p>
+            </div>
+        </>
   )
 }
 
