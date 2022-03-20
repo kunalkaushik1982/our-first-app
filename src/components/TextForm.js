2703276529b094a60862a0c2202bf59be860a3ad
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-03-20 16:28:45 +0530

    Implemented Toggle dark and light mode with usestate hooks

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,7 +7,11 @@ export default function TextForm(props) {
         let newText=text.toUpperCase()
         setText(newText)
     }
-
+    const handleClearClick = ()=>{
+        console.log("Clear Text Area")
+        let newText=''
+        setText(newText)
+    }
     const handleLoClick = ()=>{
         let newText=text.toLowerCase()
         setText(newText)
@@ -28,10 +32,11 @@ export default function TextForm(props) {
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
             </div>
             <div className="conatiner my-3">
                <h2>Your Text Summary</h2> 
-               <p> {text.split(" ").length-1} words and {text.length} characters</p>
+               <p> Word Count: {text.split(" ").length} | Character Count: {text.length} | Line Count: {text.split("\n").length}</p>
                <p>{0.008*text.split(" ").length} Minutes read</p>
                <h2>Preview</h2>
                <p>{text}</p>
