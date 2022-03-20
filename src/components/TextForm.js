2382271f0262f29faf8950611b3ab80fc8c8cdd5
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-03-20 18:09:08 +0530

    Toggle theme of background using usestate hooks

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,24 +22,38 @@ export default function TextForm(props) {
         setText(event.target.value)        
     }
 
+    const handleCopyClick = ()=>{
+        let newText=document.getElementById("myBox")
+        newText.select();
+        navigator.clipboard.writeText(newText.value)
+    }
+
+    const handleExtraSpace = ()=>{
+        let newText=text.split(/[ ]+/)
+        setText(newText.join(" "))
+    }
+
   const [text,setText]=useState("")
   return (
       <>
-            <div className="container">
+            <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">                
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"
+                    style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} ></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
                 <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Space</button>
             </div>
-            <div className="conatiner my-3">
+            <div className="conatiner my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
                <h2>Your Text Summary</h2> 
                <p> Word Count: {text.split(" ").length} | Character Count: {text.length} | Line Count: {text.split("\n").length}</p>
                <p>{0.008*text.split(" ").length} Minutes read</p>
                <h2>Preview</h2>
-               <p>{text}</p>
+               <p>{text.length>0?text:"Enter something to preview:"}</p>
             </div>
         </>
   )
